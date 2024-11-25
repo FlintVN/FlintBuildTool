1813d777dd8ff6e823fa0ced5b2ce94950eaeb6f
Thanh Tùng <electricthanhtung@gmail.com>
2024-11-25 11:21:08 +0700

    Initial commit

---
+++ FlintBuildTool/FlintMerge.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FlintBuildTool {
+    internal static class FlintMerge {
+        private class FirmwareBinary {
+            public FirmwareBinary(int offset, byte[] data) {
+                Offset = offset;
+                Data = data;
+            }
+
+            public int Offset {
+                get;
+            }
+
+            public byte[] Data {
+                get;
+            }
+        }
+
+        public static void MergeBinary(string[] args) {
+            List<FirmwareBinary> binarys = new List<FirmwareBinary>();
+            string outFileName = "MergedBinary.bin";
+            int index = 0;
+            int totalSize = 0;
+            while(index < args.Length) {
+                if(args[index] == "-o") {
+                    index++;
+                    outFileName = args[index++];
+                }
+                else {
+                    int offset = Convert.ToInt32(args[index++], 16);
+                    string filePath = args[index++];
+                    byte[] data;
+                    if(File.Exists(filePath))
+                        data = File.ReadAllBytes(filePath);
+                    else
+                        throw new FileNotFoundException("", filePath);
+                    FirmwareBinary binary = new FirmwareBinary(offset, data);
+                    int endOfAddr = binary.Offset + binary.Data.Length;
+                    if(totalSize < endOfAddr)
+                        totalSize = endOfAddr;
+                    binarys.Add(binary);
+                }
+            }
+
+            byte[] mergeBinary = new byte[totalSize];
+            for(int i = 0; i < mergeBinary.Length; i++)
+                mergeBinary[i] = 0xFF;
+            for(int i = 0; i < binarys.Count; i++)
+                Array.Copy(binarys[i].Data, 0, mergeBinary, binarys[i].Offset, binarys[i].Data.Length);
+            File.WriteAllBytes(outFileName, mergeBinary);
+            Console.WriteLine("Created " + outFileName);
+        }
+    }
+}
---
+++ FlintBuildTool/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FlintBuildTool {
+    internal static class Program {
+        static void Main(string[] args) {
+            if(args == null || args.Length == 0)
+                ShowHelp();
+            else {
+                try {
+                    if(args[0] == "merge")
+                        FlintMerge.MergeBinary(args.Skip(1).ToArray());
+                    else if(args[0] == "utf8crc")
+                        Uft8Crc.Calculate((args != null && args.Length > 1) ? args.Skip(1).ToArray() : null);
+                    else if(args[0] == "help")
+                        ShowHelp();
+                    else
+                        Console.WriteLine("Unknow command \"" + args[0] + "\"");
+                }
+                catch(FileNotFoundException ex) {
+                    Console.WriteLine("File not found: " + ex.FileName);
+                    Environment.ExitCode = 1;
+                }
+                catch(Exception ex) {
+                    Console.WriteLine(ex.Message);
+                    Environment.ExitCode = 1;
+                }
+            }
+        }
+
+        private static void ShowHelp() {
+            Console.WriteLine("merge:   Merge multiple binary files into one file.");
+            Console.WriteLine("         usage: FlintBuildTool merge [<options>] [<binary list>]");
+            Console.WriteLine("                [<options>]: -o: output file");
+            Console.WriteLine("                [<binary list>]: [<offset>] [<file name>]");
+            Console.WriteLine();
+            Console.WriteLine("utf8crc: Calculate Crc and length for FlintConstUtf8.");
+            Console.WriteLine("         usage: FlintBuildTool utf8crc [<folder list>]");
+        }
+    }
+}
